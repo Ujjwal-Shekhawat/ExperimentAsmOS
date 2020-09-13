bb89c17577a1223a0129afb58b50982e447dfb11
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-09-13 23:04:14 +0530

    Code cleanup

--- src/screen/resetTextMode.asm
+++ src/screen/resetTextMode.asm
@@ -5,13 +5,13 @@
 resetTextMode:
     ;;; Set text mode
     mov ah, 0x00            ; int 0x10/ 0x00 = Set video mode
-    mov al, 0x03            ; Setting video mode to regulair text mode of 80x25
+    mov al, 0x01            ; Setting video mode to regulair text mode of 80x25
     int 0x10
 
     ;;; Set color
     mov ah, 0x0B
     mov bh, 0x00
-    mov bl, 0x01
+    mov bl, 0x00
     int 0x10
 
     ret
